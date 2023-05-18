26a589397aa716c466c166e8257b4616e7084bd8
Syntx-09 <116119936+Syntx-09@users.noreply.github.com>
2023-05-18 04:39:57 +0100

    Delete func.php

--- auth/func.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "http://" . $_SERVER['HTTP_HOST'] . dirname($_SERVER['PHP_SELF']) . "/reset_pass.php?token=";
-function name($fname, $lname)
-{
-    echo "My first name is $fname while <b> my last name is $lname </b>. Thank you";
-
-}
-
-
